300b4adee3544e554fea956b4fc853d0a4cf77c4
Annanya7 <78608477+Annanya7@users.noreply.github.com>
2022-01-16 14:45:53 +0530

    Day-3 Calculate the sum of odd numbers from 1 to n.Edited

---
+++ src/Q24_sumOdd.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Q24_sumOdd
+{
+    public static void calSumOdd(int num) {
+        int i, j;
+        int sum = 0;
+        for (i = 1; i <= num; i++) {
+            if (i % 2 != 0) {
+                sum = sum + i;
+                System.out.println(i);
+            }
+        }
+        System.out.println("Sum of odd numbers is" + sum);
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of values");
+        int n = sc.nextInt();
+        calSumOdd(n);
+    }
+}
