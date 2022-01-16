e5d1e577094871baccf0bacb54c701569361d9fc
Annanya7 <78608477+Annanya7@users.noreply.github.com>
2022-01-16 19:53:12 +0530

    Day-3 :Infinite do while loop

---
+++ src/Q28_infiniteLoop.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Q28_infiniteLoop
+{
+    public static void main(String[] args)
+    {
+        int i;
+        do {
+            System.out.println("helloo");
+        }while(true);
+
+    }
+}
