79852b40ac2fa1b04160101b0a7a063eac3a3c4c
Annanya7 <78608477+Annanya7@users.noreply.github.com>
2022-01-21 08:40:01 +0530

    Day:6

--- src/array_Q1.java
+++ src/array_Q1.java
@@ -6,11 +6,11 @@ public class array_Q1
         int i;
         Scanner sc=new Scanner(System.in);
         int size = sc.nextInt();
-        String name []= new String[size];
-        for(i=0;i<=size;i++)
+        String[] name = new String[size];
+        for(i=0;i<= name.length;i++)
         {
             Scanner s = new Scanner(System.in);
-            name[i] = sc.nextLine();
+            name[i] = s.nextLine();
             System.out.println(name[i]);
 
         }
