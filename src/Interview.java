8bd53b8308890b78679e966b00cc52364f9dd8c8
Annanya7 <78608477+Annanya7@users.noreply.github.com>
2022-02-05 11:18:34 +0530

    Overloading the main function understanding boolean in for loop

--- src/Interview.java
+++ src/Interview.java
@@ -1,10 +1,14 @@
 public class Interview
 {
-    public static void main(String args) {
+    public static void main(String[] args)
+    {
+        for(int i = 0; true; i++) {
+            System.out.println("Hello");
+            break;
+        }
 
     }
-
-    public static void main(String[] args) {
+    public static void main(String args) {
         System.out.println("hello");
     }
 }
