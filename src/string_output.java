1cbb9baaba883d4b7a4ef99ef453cf375b26d9d7
Annanya7 <78608477+Annanya7@users.noreply.github.com>
2022-02-05 10:56:16 +0530

    String output based questions - interview

---
+++ src/string_output.java
@@ -0,0 +1,9 @@
+public class string_output
+{
+    public static void main(String[] args)
+    {
+        System.out.print('j' + 'a' + 'v' + " \n");
+        System.out.print("j" + "a" + "v");
+
+    }
+}
