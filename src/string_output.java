c570f1135a5b39b3f3070d4f2f4511bb193bef4f
Annanya7 <78608477+Annanya7@users.noreply.github.com>
2022-02-05 11:04:14 +0530

    String output based questions - interview added another question

--- src/string_output.java
+++ src/string_output.java
@@ -2,8 +2,28 @@ public class string_output
 {
     public static void main(String[] args)
     {
+        // Q1
+        System.out.println("******************* Q1 *******************");
         System.out.print('j' + 'a' + 'v' + " \n");
         System.out.print("j" + "a" + "v");
-
+        // Q2
+        System.out.print("******************* Q2********************");
+        Integer num1 = 100;
+        Integer num2 = 100;
+        Integer num3 = 500;
+        Integer num4 = 500;
+        // int value - -128 to 127 - same reference but value above that it changes the reference
+        if(num1==num2){
+            System.out.println("num1 == num2");
+        }
+        else{
+            System.out.println("num1 != num2");
+        }
+        if(num3 == num4){
+            System.out.println("num3 == num4");
+        }
+        else{
+            System.out.println("num3 != num4");
+        }
     }
 }
