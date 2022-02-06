1e837fd0457055a0a0c14da8b15fbd19c5142eb8
Annanya7 <78608477+Annanya7@users.noreply.github.com>
2022-02-06 20:17:34 +0530

    interview question 2

---
+++ src/interview_2.java
@@ -0,0 +1,9 @@
+public class interview_2
+{
+    int x = 10;
+    public static void main(String[] args)
+    {
+        interview_2 t = new interview_2();
+        System.out.println(t.x);
+    }
+}
