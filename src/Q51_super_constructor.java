4d1b5cb92252992fb7abe48234f47d603960b3a0
Annanya7 <78608477+Annanya7@users.noreply.github.com>
2022-02-07 16:28:22 +0530

    Multi level inheritance -2

---
+++ src/Q51_super_constructor.java
@@ -0,0 +1,30 @@
+public class Q51_super_constructor
+{
+    Q51_super_constructor()
+    {
+        System.out.println(" base class constructor");
+    }
+}
+class derived extends Q51_super_constructor
+{
+    derived()
+    {
+        super();
+        System.out.println("dervied class constructor ");
+    }
+    derived(int x , int y)
+    {
+        super();
+        System.out.println("parameters = " + (x + y));
+    }
+}
+class Result
+{
+    public static void main(String[] args)
+    {
+        derived obj = new derived();
+        derived obj1 = new derived(20,30);
+
+    }
+}
+
