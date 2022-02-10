f06d407caf9535da087f88040796a22edd517190
Annanya7 <78608477+Annanya7@users.noreply.github.com>
2022-02-10 17:41:00 +0530

    String q1

---
+++ src/Strings_Q1.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+public class Strings_Q1
+{
+    public static void main(String args[]) {
+        String s = "Ananya ";
+        String s1 = " Ananya";
+        System.out.println(s1==s);
+
+    }
+}
